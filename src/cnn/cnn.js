c645e86131894867969e94490df0f19ff2b2fa96
Elias Ortega <eliasortega2011@gmail.com>
2023-07-18 20:40:16 -0400

    conexion bd remota y comando start

--- src/cnn/cnn.js
+++ src/cnn/cnn.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 
 //Instancia de conexion. user:postgres,password, localhost, nombre de base de datos. 
- export const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/casoEstudiantes');
+ export const sequelize = new Sequelize('postgres://postgres:gIIMHc4KmEopnhnqq0qk@containers-us-west-111.railway.app:6112/railway');
 
+ 
