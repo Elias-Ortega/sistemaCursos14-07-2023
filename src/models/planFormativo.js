d17869bcc6a5827f238e8c68e8d3b4f02ceb342c
Elias Ortega <eliasortega2011@gmail.com>
2023-07-21 20:53:07 -0400

    relacion de plan a curso

--- src/models/planFormativo.js
+++ src/models/planFormativo.js
@@ -1,33 +1,52 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../cnn/cnn.js";
+import { Curso } from "./cursos.js";
 
 
 const PlanFormativo = sequelize.define('PlanFormativo', {
-
     codigo_plan_formativo: {
         type: DataTypes.STRING(30),
         primaryKey: true,
     },
-
     descripcion: {
         type: DataTypes.STRING(250)
-
     },
     duracion_horas: {
         type: DataTypes.INTEGER
     }
 }, {
-    tableName: 'plan_formativo', //esta configuracion es para que encuentre la tabla si no la encuentra
-    createdAt: false,
+    tableName: 'plan_formativo',
+    createdAt: false,//se hace para evitar que el modelo cree estas dos tablas automaticamente en la base
     updatedAt: false
 });
 
-export {
+PlanFormativo.hasMany(Curso, {
+    foreignKey: 'codigo_plan_formativo',
+    sourceKey: 'codigo_plan_formativo'
+});
+
+Curso.belongsTo(PlanFormativo, {
+    foreignKey: 'codigo_plan_formativo',
+    targetKey: 'codigo_plan_formativo'
+});
+
+
+
+export{
     PlanFormativo
 }
 
-/* const listadoPlanes = await PlanFormativo.findAll();
-
-listadoPlanes.forEach( () => {
-    console.log(plan.dataValues);
+// Lazy Loading: carga diferida: carga perezosa
+/* const plan = await PlanFormativo.findByPk('A1');
+console.log(plan.dataValues);
+const cursosPlan = await plan.getCursos();
+cursosPlan.forEach(curso => {
+    console.log(curso.dataValues);
 }); */
+
+// Eager Loading: carga ansiosa
+/* const plan = await PlanFormativo.findByPk('A1', {
+    include: Curso
+});
+
+console.log(plan.dataValues); */
